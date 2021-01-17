bc2cb7326d425be1e7d986e7ccc1d4e275d34475
abhivk23 <abhivk23@gmail.com>
2021-01-17 06:21:55 -0600

    rescaled mapping, output generated playlist url

--- client.py
+++ client.py
@@ -12,8 +12,7 @@ from sentiment import analyze_sentiment
 SPOTIFY_CLIENT_ID="56c1ae8e401640d6b90b8066c3821f95"
 SPOTIFY_CLIENT_SECRET="665045e7a1a44db7ae68b220bf1f12a3" # !!! hide key
 SPOTIPY_REDIRECT_URI="https://abhivk23.github.io"
-REQUESTED_SCOPES="user-library-read, user-follow-read"
-
+REQUESTED_SCOPES="user-library-read, user-follow-read, playlist-modify-public, playlist-modify-private"
 if len(sys.argv)>1:
     username = sys.argv[1]
 else:
@@ -34,23 +33,27 @@ sp_client = spotipy.client.Spotify(auth=token)
 
 
 ext_vibes = analyze_sentiment("I'm kinda nervous about this demonstration I'm doing right now. We spent the past weekend working on this and I hope I don't mess it up.")
-print("Building your playlist...")
-#ext_vibes = [-0.44, 2.79]
-#print("The external_vibes: " + ext_vibes + "which means we're feeling ~negative vibes, with ~low energy (sad)")
+#ext_vibes = [-0.20000000298023224, 0.5]
 
 ## Map [strength, magnitude] --> [valence, energy] assuming unif. dist.
 ## strength: [-1.0, 1.0] ; magnitude: [0.0 , ~13] (technically LUB=#inf, but approx. for relevant input)
 ## valence: [0.0, 1.0] ; energy: [0.0 , 1.0]
 ext_vibes[0] = (1.0+ext_vibes[0])/2.0
-ext_vibes[1] = (ext_vibes[1])/13.0
+ext_vibes[1] = (ext_vibes[1])/4.0
 
+print("The external_vibes [{stre:.4f}, {mag:.4f}] correspond to [insert emotions here]".format(stre=ext_vibes[0], mag=ext_vibes[1]))
+
+print("Building your playlist...")
 from recommendation import Recommender
-Rec = Recommender(sp_user, sp_client, ext_vibes)
+Rec = Recommender(sp_user, sp_client, ext_vibes, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
 uri = Rec.generate_track_rec()
 track_id = uri.split(":")[2] # TRACK ID
 artists = [artist['name'] for artist in sp_client.track(track_id)['artists']]
+print("Your VibeCheck recommendation is: " + sp_client.track(track_id)['name'] + " by " + artists[0]) # expand to get all artists
 
-print("The current vibe is: " + sp_client.track(track_id)['name'] + " by " + artists[0]) # expand to get all artists
+# Generate a shareable playlist of some other similarly feeling songs
+playlist_url = Rec.generate_playlist(username, "vibecheck123", 10, "that's an awfully hot coffee pot")
+print("Check this playlist out to continue to ride the vibe: " + playlist_url)
 
 """
 ## Example Client API calls
--- recommendation.py
+++ recommendation.py
@@ -1,6 +1,8 @@
 ## Recommendation module linking text_sentiment score w sentiment scores from user_library
 import spotipy
+import spotipy.oauth2
 import math
+import os
 
 def distance_between(P1, P2):
         x_dist = P2[0] - P1[0]
@@ -8,16 +10,21 @@ def distance_between(P1, P2):
         return math.sqrt(x_dist ** 2 + y_dist ** 2)
 
 class Recommender:
-    def __init__(self, sp_user, sp_client, curr_state):
+    def __init__(self, sp_user, sp_client, curr_state, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET):
         """
             params: spotipy.Spotify, spotipy.client.Spotify, [valence, energy] --> Recommender
         """
+        self.user_id = SPOTIFY_CLIENT_ID
+        self.secret_id = SPOTIFY_CLIENT_SECRET
         self.user = sp_user
         self.client = sp_client
         self.current_state = curr_state ## strength: [0.0, 1.0] ; magnitude: [0.0 , 1.0]
+        self.last_rec_tracks = []
         self.followed_artists = self.user.current_user_followed_artists(limit=10, after=None)['artists']['items'] # want to sort by most preferred, rn j first 20
 
-    # want to make this generic for any 
+    # want to make this generic for any set of tracks we might want to test
+    ## in the future, use Browse API Recommendations call as a benchmark to the metrics we test
+    ## for like science ig
     def generate_trackSentiment_distances(self, n):
         """
             Calculate distance between current_state and set of tracks' (followed_artists) sentiment
@@ -34,12 +41,53 @@ class Recommender:
 
                 distances_from_currentState.append(distance_between(self.current_state, sentiment_point))
                 tracks.append(track['uri'])
-        return tracks, distances_from_currentState
+        self.last_rec_tracks = sorted(tracks)
+        return distances_from_currentState
 
     def generate_track_rec(self):
         """
             --> track_uri (song recommendation) 
         """
-        tracks, distances = self.generate_trackSentiment_distances(3)
+        distances = self.generate_trackSentiment_distances(3)
         min_index = distances.index(min(distances))
-        return tracks[min_index]
+        return self.last_rec_tracks[min_index]
+
+    # generalize for other identifiers maybe
+    def generate_playlist(self, token, pl_name, pl_len, pl_desc):
+        """
+            track_id (song rec) --> playlist_URL (?) (song recommendation) 
+        """
+
+        ## !! need to reauthorize the client for some reason, this should be handled better for sure
+        SPOTIFY_CLIENT_ID="56c1ae8e401640d6b90b8066c3821f95"
+        SPOTIFY_CLIENT_SECRET="665045e7a1a44db7ae68b220bf1f12a3" # !!! hide key
+        SPOTIPY_REDIRECT_URI="https://abhivk23.github.io"
+        REQUESTED_SCOPES="user-library-read, user-follow-read, playlist-modify-public, playlist-modify-private"
+        username = "npby7v4xwgsty64kobmk9xb3w"
+
+        # Generate Spotify auth_token
+        try:
+            token = spotipy.util.prompt_for_user_token(username, scope=REQUESTED_SCOPES, client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI)
+
+        except:
+            os.remove(f".cache-{username}")
+            token = spotipy.util.prompt_for_user_token(username, scope=REQUESTED_SCOPES, client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI)
+        
+        # Configure user/client
+        sp_user = spotipy.Spotify(auth=token)
+        sp_client = spotipy.client.Spotify(auth=token)
+        user_id = sp_user.me()['id']
+
+        # build playlist
+        sp_client.user_playlist_create(user_id, pl_name, public=True, collaborative=False, description=pl_desc)
+        playlist = self.client.user_playlists(user_id)['items'][0]
+        url = playlist['external_urls']['spotify']
+        playlist_id = playlist['id']
+
+        # add top "pl_len" tracks minimized w.r.t distance to current_state to playlist
+        sp_user.playlist_add_items(playlist_id, self.last_rec_tracks[:pl_len])
+
+        return url
+
+
+
