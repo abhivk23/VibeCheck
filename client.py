0ab577e2fa084a9990e05a96ea6cad04afd45717
abhivk23 <abhivk23@gmail.com>
2021-01-16 23:25:42 -0600

    fetch data, generate track rec from artists

--- client.py
+++ client.py
@@ -2,13 +2,12 @@
 import os, sys
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import recommendation as rec
 
 ## Configure authentication 
 SPOTIFY_CLIENT_ID="56c1ae8e401640d6b90b8066c3821f95"
-SPOTIFY_CLIENT_SECRET=""
+SPOTIFY_CLIENT_SECRET="665045e7a1a44db7ae68b220bf1f12a3"
 SPOTIPY_REDIRECT_URI="https://abhivk23.github.io"
-REQUESTED_SCOPES="user-library-read"
+REQUESTED_SCOPES="user-library-read, user-follow-read"
 
 if len(sys.argv)>1:
     username = sys.argv[1]
@@ -22,19 +21,37 @@ except:
     os.remove(f".cache-{username}")
     token = spotipy.util.prompt_for_user_token(username, scope=REQUESTED_SCOPES, client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI)
 
-sp = spotipy.Spotify(auth=token)
-sp_client = spotipy.client.Spotify(auth=token)
-
 ## Take textual sentiment input and generate song recommendation
 #text_sentiment = input("TEXT INPUT: ")
 #print(text_sentiment)
-Rec = rec.Recommendation(sp)
-uri = Rec.user_lib[0]['track']['uri']
-track_id = uri.split(":")[2]
-print(sp_client.track(track_id)['name'], "\n", sp_client.audio_features([track_id]))
 
+sp_user = spotipy.Spotify(auth=token)
+sp_client = spotipy.client.Spotify(auth=token)
 
-""" for song in Rec.user_lib:
+from recommendation import Recommender
+current_state = [0,0]
+Rec = Recommender(sp_user, sp_client, current_state)
+uri = Rec.generate_track_rec()
+track_id = uri.split(":")[2] # TRACK ID
+#print(sp_client.track(track_id))
+artists = [artist['name'] for artist in sp_client.track(track_id)['artists']]
+print("The current vibe is: " + sp_client.track(track_id)['name'] + " by " + artists[0]) # expand to get all artists
+
+""" 
+## Client API calls
+print(sp_client.track(track_id)['name']) # TRACK NAME
+print(sp_client.audio_features([track_id])) # FEATURES 
+"""
+""" ## User API calls
+followed = sp_user.current_user_followed_artists(limit=20, after=None)['artists']['items'] ## FOLLOWED_ARTISTS (list)
+for artist in followed:
+    res = sp_client.artist_top_tracks(artist['uri'])
+
+    for track in res['tracks'][:10]:
+        print(track['uri']) """
+""" 
+for song in Rec.user_lib:
     uri = song['track']['uri']
     track_id = uri.split(":")[2]
-    print(sp_client.audio_analysis(track_id)) """
+    print(sp_client.audio_analysis(track_id)) 
+"""
--- recommendation.py
+++ recommendation.py
@@ -1,15 +1,52 @@
 ## Recommendation module linking text_sentiment score w sentiment scores from user_library
 import spotipy
-#from all.ai import sentiment
+import math
 
-class Recommendation:
-    def __init__(self, user_sp):
+""" class User:
+    def __init(self, user_sp, client_sp):
         self.sp = user_sp
-        self.user_lib = self.sp.current_user_saved_tracks()['items']
+        self.client = client_sp """
+
+def distance_between(P1, P2):
+        x_dist = P2[0] - P1[0]
+        y_dist = P2[1] - P1[1]
+        return math.sqrt(x_dist ** 2 + y_dist ** 2)
+
+class Recommender:
+    def __init__(self, sp_user, sp_client, curr_state):
+        """
+            User, [valence, rel_activation] --> 
+        """
+        self.user = sp_user
+        self.client = sp_client
+        self.current_state = curr_state
+        self.followed_artists = self.user.current_user_followed_artists(limit=10, after=None)['artists']['items'] # want to sort by most preferred, rn j first 20
+
+        #self.user_lib = self.sp.current_user_saved_tracks()['items']
         #self.lib_sentiment = generate_library_sentimenet()
 
-    def generate_library_sentiment(self):
+    def generate_trackSentiment_distances(self, n):
         """
-        Return dictionary of TRACK-->TRACK_SENTIMENT.
+           n (top_n) --> [list[distances], list[list[valence, liveliness]]] (2x2 matrix of artists' top n songs' features)
         """
-        #for song in self.user_lib:
+        tracks = []
+        distances_from_currentState = []
+        for artist in self.followed_artists:
+            artist_top_tracks = self.client.artist_top_tracks(artist['uri'])
+            top_n = artist_top_tracks['tracks'][:n] # top number is arbitrary
+            for track in top_n:
+                audio_features = self.client.audio_features([track['uri']])[0]
+                sentiment_point = [audio_features['valence'], audio_features['liveness']] # need to re-map to common basis
+
+                distances_from_currentState.append(distance_between(self.current_state, sentiment_point))
+                tracks.append(track['uri'])
+
+        return tracks, distances_from_currentState
+
+    def generate_track_rec(self):
+        """
+            --> track_uri (song recommendation) 
+        """
+        tracks, distances = self.generate_trackSentiment_distances(3)
+        min_index = distances.index(min(distances))
+        return tracks[min_index]
